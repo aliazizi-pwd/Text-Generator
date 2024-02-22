4af3896fd69fbe54af356c6b5877eac3e5d97db4
Ali Azizi <aliazizi.4973@gmail.com>
2024-02-22 18:58:14 +0330

    export data

--- source/javascript/app.js
+++ source/javascript/app.js
@@ -7,3 +7,10 @@ const btnMinus = $.querySelector(".btn-minus");
 const filterTag = $.querySelector(".filterTag");
 const btnEraser = $.querySelector(".btn-eraser");
 const showTextGenerator = $.querySelector(".show-textGenerator");
+
+
+
+// import * data from "data.js" :)
+import { generateEnglish , generatePersian } from "./data.js";
+console.log(generateEnglish);
+console.log(generatePersian);
