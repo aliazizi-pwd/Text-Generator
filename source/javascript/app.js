e5d8b0f67a6cbd7fa379d727e9b49aeda6e0a7f4
Ali Azizi <aliazizi.4973@gmail.com>
2024-02-22 19:13:19 +0330

    codeing part plus and miuns count text generate

--- source/javascript/app.js
+++ source/javascript/app.js
@@ -1,4 +1,6 @@
 let $ = document;
+let countText = 0;
+
 const btnGenerateText = $.querySelector(".btn-generate");
 const btnEnglish = $.querySelector(".btn-en");
 const btnPersian = $.querySelector(".btn-Pe");
@@ -6,11 +8,33 @@ const btnPlus = $.querySelector(".btn-plus");
 const btnMinus = $.querySelector(".btn-minus");
 const filterTag = $.querySelector(".filterTag");
 const btnEraser = $.querySelector(".btn-eraser");
+const inputCountText = $.querySelector(".input-count");
 const showTextGenerator = $.querySelector(".show-textGenerator");
 
 
 
 // import * data from "data.js" :)
 import { generateEnglish , generatePersian } from "./data.js";
-console.log(generateEnglish);
-console.log(generatePersian);
+
+
+// plus count text generate 
+function plusCountGenerate () {
+    countText++;
+    inputCountText.value = countText;
+}
+
+// Miuns count text generate 
+function minusCountGenerate () {
+    countText--;
+    if (countText < 0) {
+        countText = 0;
+    }
+    inputCountText.value = countText;
+}
+
+
+
+
+// add event listener button element html
+btnPlus.addEventListener("click",plusCountGenerate);
+btnMinus.addEventListener("click",minusCountGenerate);
