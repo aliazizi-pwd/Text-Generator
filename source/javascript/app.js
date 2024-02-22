3f876c40421da48863065e23095e1c0f97cd0136
Ali Azizi <aliazizi.4973@gmail.com>
2024-02-23 00:09:26 +0330

    codeing part text generate English

--- source/javascript/app.js
+++ source/javascript/app.js
@@ -1,6 +1,6 @@
 let $ = document;
 let countText = 0;
-let selection = "";
+let selection = "English";
 
 const btnGenerateText = $.querySelector(".btn-generate");
 const btnEnglish = $.querySelector(".btn-en");
@@ -37,21 +37,45 @@ function minusCountGenerate () {
 function SelectingLanguage () {
     btnsLanguage.forEach(function (btn) {
         btn.addEventListener("click", function (e) {
-            // selection = e.target.dataset.name;
+            selection = e.target.dataset.name;
             for (let i = 0; i < btnsLanguage.length; i++) {
                 btnsLanguage[i].classList.replace("btn-warning", "btn-light");
             }   
-            
+
             btn.classList.replace("btn-light","btn-warning");
         });
     });
 }
 
 
+// text Generator Handler
+function textGeneratorHandler () {
+    let countTextValue = inputCountText.value;
+    let randomTextGenerate;
+    
+    
+    if (selection === "English") {
+        if (countTextValue === "") {
+            randomTextGenerate = generateEnglish[Math.floor(Math.random() * generateEnglish.length)];
+            showTextGenerator.innerHTML = `<p class="p-3 text-start">${randomTextGenerate}</p>`;
+        } else {
+            let newTextData = generateEnglish.slice(0 , countTextValue);
+            
+            let resultGenerateFinal = newTextData.map(function (text) {
+                return `<p class="p-3 text-start">${text}</p>`;
+            }).join(" ");
+
+            
+            showTextGenerator.innerHTML = resultGenerateFinal;
+        }
+    }
+}
+
 
 
 // add event listener button element html
 btnPlus.addEventListener("click",plusCountGenerate);
 btnMinus.addEventListener("click",minusCountGenerate);
+btnGenerateText.addEventListener("click",textGeneratorHandler);
 // selected language
 SelectingLanguage();
